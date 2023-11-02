c1c95a4d2117571e0011623d61b5507c41bf3e56
Andrea Shepherd <118775368+andreashep@users.noreply.github.com>
2023-11-02 15:11:07 -0700

    Tyrannasaurus is now cute and finished.

--- script.js
+++ script.js
@@ -4,22 +4,36 @@
 
 // 2. Use querySelector to create a variable that contains the page's h1 title, and change the title using textContent into something a bit nicer.
 
+const pageTitle = document.querySelector('#pageTitle');
+pageTitle.textContent = "My arms are too short!";
+
 // 3. Use querySelector to create a variable that contains the dinosaur image, and change the src to "./images/cute-dino.jpeg" so he doesn't look so mean. You can change an item's src by just calling the src method (item.src = "new_src";)
-
+const dinoImg = document.querySelector('#dinoImg');
+dinoImg.src = "./images/cute-dino.jpeg";
 // 4. Use querySelector to create a variable for the document body, and change the background color to something less harsh and intense.
-
+var background = document.body;
+background.style.backgroundColor = "aqua";
 // 5. Modify the following array to add at least 3 facts about the T. Rex that aren't so frightening. They don't need to be real facts.
 
 let dinoFacts = [
-    "Fact 1",
-    "Fact 2",
-    "Fact 3"
+    "This t-rex has lot\'s of friends in the pteredactyles and a brontosaurus.",
+    "This little guy\'s name is Harry",
+    "He wants to be a fire fighter when he grows up."
 ]
+
 
 // 6. Replace the facts on the page with our new array of facts:
 // 6.1 First, use querySelector to create a variable for the list.
-
+let cuteGuy = document.querySelector('#dinoFacts');
 // 6.2 Then, remove all of the current content by setting your list variable's innerHTML to a blank string (variable.innerHTML = "")
+(cuteGuy.innerHTML = "")
 
 // 6.3 Then loop through the dinoFacts array, and appendChild each fact to the list variable. Remember you can loop through an array by using array.length to get the total number of items in the array to end the loop;
+for (let i = 0; i < dinoFacts.length; i++) {
+    let currentFact = dinoFacts[i];
+    const newList = document.createElement("li");
+    newList.textContent = currentFact;
+    cuteGuy.appendChild(newList);
+}
+
 
